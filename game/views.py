e2d3ed92605befee2be8d3c4c9b2ced5f4b8a6ba
Wei Wei <2312052806@qq.com>
2023-06-24 09:11:14 +0800

    send background.gif

--- game/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    line1 = '<h1 style="text-align: center">Warlock wars</h1>'
-    line2 = '<img src="https://img0.baidu.com/it/u=2857096089,2095994689&fm=253&fmt=auto&app=138&f=JPEG?w=1070&h=500" width=1500>'
-    return HttpResponse(line1 + line2)
