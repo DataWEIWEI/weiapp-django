aedd63769ce8ae743242cb43c3ad41f0b8835a45
Wei Wei <2312052806@qq.com>
2023-06-25 14:45:51 +0800

    achieve prepare work

--- game/urls/index.py
+++ game/urls/index.py
@@ -4,6 +4,6 @@ from game.views.index import index
 urlpatterns = [
     path('', index, name='index'),
     path('menu/', include('game.urls.menu.index')),
-    path('playground/', include('game.urls.menu.playground')),
-    path('settings/', include('game.urls.menu.settings'))
+    path('playground/', include('game.urls.playground.index')),
+    path('settings/', include('game.urls.settings.index'))
 ]
