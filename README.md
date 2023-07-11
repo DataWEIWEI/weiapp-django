493389f34963cba638c91ec5de4e0ab7d1add43a
Wei Wei <2312052806@qq.com>
2023-07-11 10:18:52 +0800

    normalizing unit

--- README.md
+++ README.md
@@ -2,8 +2,6 @@ A DJANGO PROJECT.
 ```shell
 // start nginx service
 sudo /etc/init.d/nginx start
-
-uwsgi --ini scripts/uwsgi.ini
 ```
 
 if you are in debug, please change `DEBUG=TRUE` in settings.py file.
@@ -14,10 +12,12 @@ python3 manage.py migrate
 
 python3 manage.py shell
 
+> load uwsgi
 uwsgi --ini scripts/uwsgi.ini
 uwsgi --reload uwsgi/uwsgi.pid
 uwsgi --stop uwsgi/uwsgi.pid
 
+> introductions of files
 views: function realization and data processing
 
 urls: modify router
--- static/js/src/settings/zbase.js
+++ static/js/src/settings/zbase.js
@@ -245,9 +245,8 @@ class Settings {
     acapp_login(appid, redirect_uri, scope, state) {
         let outer = this;
         this.root.AcWingOS.api.oauth2.authorize(appid, redirect_uri, scope, state, function (resp) {
-            console.log(resp.result);
+            console.log(resp);
             if (resp.result === 'success') {
-                console.log('aaaaaa');
                 outer.username = resp.username;
                 outer.photo = resp.photo;
                 outer.hide();
