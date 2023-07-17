743e0d9b10a9367734520c409ac9e33c676c12fd
Wei Wei <2312052806@qq.com>
2023-07-17 15:04:49 +0800

    add calculator

--- game/urls/calculator/index.py
+++ game/urls/calculator/index.py
@@ -1,6 +1,9 @@
-from django.urls import path, include
+from django.urls import path, re_path
 from game.views.calculator.getinfo import InfoView
 from game.views.calculator.register import Register
+from game.views.calculator.logout import Logout
+from game.views.calculator.get_status import get_status
+from game.views.calculator.index import index
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -11,4 +14,7 @@ urlpatterns = [
     path("token/refresh", TokenRefreshView.as_view(), name="calculator_token_refresh"),
     path("getinfo/", InfoView.as_view(), name="calculator_getinfo"),
     path("register/", Register, name="calculator_register"),
+    path("logout/", Logout, name="calculator_logout"),
+    path("get_status/", get_status, name="get_status"),
+    re_path(r'.*', index, name='calculator_index')
 ]
